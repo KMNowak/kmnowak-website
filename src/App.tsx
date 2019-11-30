52a06e67d3ed1a7aeb9ba0851a469881054c4c3c
KMNowak <krzysztofmareknowak@gmail.com>
2019-11-30 16:04:44 +0100

    add Helmet for meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import './styles/styles.sass'
 import { Start, About, Skills, Footer, FixedNav } from './sections'
 
 const App: React.FC = () => {
     return (
         <div>
+            <Helmet>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <meta name="description" content="" />
+                <meta name="viewport"
+                      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+                <meta name="Keywords" content="kmnowak, nodejs, developer, webdeveloper, react, reactnative" />
+                <title>kmnowak - NodeJS developer</title>
+            </Helmet>
             <FixedNav />
             <Start />
             <About />
