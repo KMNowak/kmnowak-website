a07a3a3e5c4ce5072e5d7cb22f7fc5b7e7f38173
KMNowak <krzysztofmareknowak@gmail.com>
2019-12-07 18:16:10 +0100

    fix head

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
 import Radium from 'radium'
 import './styles/styles.sass'
 import { Start, About, Skills, Footer, FixedNav, Contact, Experience } from './sections'
@@ -7,18 +6,6 @@ import { Start, About, Skills, Footer, FixedNav, Contact, Experience } from './s
 const App: React.FC = () => {
     return (
         <div>
-            <Helmet>
-                <meta name="viewport"
-                      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-                <meta name="Keywords" content="kmnowak, Krzysztof Nowak, javascript, webdeveloper, nodejs, ReactNative, React" />
-                <title>kmnowak - JavaScript developer</title>
-                <link rel="icon" href="./public/favicon.ico" />
-                <meta name="theme-color" content="#000000" />
-                <link rel="apple-touch-icon" href="./public/icon-152.png" />
-                <link rel="manifest" href="./public/manifest.json" />
-                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-                <title>kmnowak - JavaScript developer</title>
-            </Helmet>
             <FixedNav />
             <Start />
             <About />
